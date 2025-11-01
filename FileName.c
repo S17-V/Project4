2f934b427a35bb39b88dd7141429816bc35ba9f4
S17-V <1583355176@qq.com>
2025-11-01 17:33:31 +0800

    Add files via upload

--- FileName.c
+++ FileName.c
@@ -1,22 +1,14 @@
 #include<stdio.h>
-#include<string.h>
-#include <stdlib.h>
 int main() {
-	int m,sum,n;
-	m = 100;
-	do {
-		char buf[4];
-		snprintf(buf, sizeof(buf), "%d", m);
-		char tem = buf[0];
-	    buf[0] = buf[2];
-		buf[2] = tem;
-		int n = atoi(buf);
-		sum = n + m;
-		if(sum==1333)
-		printf("a=%c,b=%c,c=%c\n", buf[2], buf[1], buf[0]);
-		m++;
-	} 
-	while (m <= 999);
-
+	int a, b, c,sum,p;
+	for (a = 0; a <= 36; a++) {
+		for (b = 0; b <= 36; b++) {
+			for (c = 0; c <= 36; c++) {
+				sum = 36;
+				if (a+b+c == 36 && 4 * a + 3 * b + c / 2==36&&c%2==0)
+					printf("%d,%d,%d\n", a, b, c);
+			}
+		}
+	}
 	return 0;
 }
